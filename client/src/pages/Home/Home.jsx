200c9c2316232971882760ed23075aa947337e8a
ASWINKUMAR C M <aswin.trikkur@gmail.com>
2024-05-04 14:07:27 +0530

    first commit

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Container } from '../../components/Container/Container'
+
+export const Home = () => {
+  return (
+
+    <div  className='home-container'>
+        <Container>
+
+            
+
+        </Container>
+    </div>
+  )
+}
